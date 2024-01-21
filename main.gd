edd6a943b55ccf2bc27c2b505f91c68c55fadf79
Emil Fahlgren <emil.fahlgren@gmail.com>
2024-01-21 15:36:24 +0100

    Add time label

--- main.gd
+++ main.gd
@@ -1,22 +1,26 @@
 extends Control
 
 var sample_pos = 0.0
-var is_playing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$TextureProgressBar.max_value = $AudioStreamPlayer.stream.get_length()
+	$TextureProgressBar.step = 0.01
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if is_playing:
+	if $AudioStreamPlayer.is_playing():
 		$TextureProgressBar.value = $AudioStreamPlayer.get_playback_position()
+		$TimeLabel.text = format_time($AudioStreamPlayer.get_playback_position())
 
 func _on_button_pressed():
-	if not is_playing:
+	if not $AudioStreamPlayer.is_playing():
 		$AudioStreamPlayer.play(sample_pos)
-		is_playing = true
 	else:
 		sample_pos = $AudioStreamPlayer.get_playback_position()
 		$AudioStreamPlayer.stop()
-		is_playing = false
+
+func format_time(seconds):
+	var minutes = int(seconds / 60)
+	var remaining_seconds = int(seconds) % 60
+	return str(minutes).pad_zeros(1) + ":" + str(remaining_seconds).pad_zeros(2)
